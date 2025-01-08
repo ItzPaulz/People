7e005d9238d4733c0d8850fa2853b7437bacefc2
ItzPaulz <paullarrea2004@gmail.com>
2025-01-07 22:37:16 -0500

    Corregido el nombre de la Base de datos

--- People/App.xaml.cs
+++ People/App.xaml.cs
@@ -2,7 +2,7 @@
 
 public partial class App : Application
 {
-    public static PersonRepository PersonRepo { get; private set; } = new PersonRepository("path_to_db");
+    public static PersonRepository PersonRepo { get; private set; } = new PersonRepository("_dbPath");
 
     public App(PersonRepository repo)
     {
